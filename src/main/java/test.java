75bdd1ddca9797b9069c9d54bae3edb13f5f265a
Razaq Taiwo sodiq <taiwosodiqrazaq@gmail.com>
2022-09-04 10:56:10 +0100

    addition using method

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,13 @@
 public class test {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        System.out.println(2+3);
+        addnum(2,5);
+
     }
-}
+        public static void addnum ( int num1, int num2){
+            int ans = num1 + num2;
+
+            System.out.print(ans);
+        }
+    }
